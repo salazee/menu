bb317837fbdf42cd771923720b35bdbd47e75393
salazee <salawudeeenzainab1@gmail.com>
2024-08-27 23:19:14 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/.style.css/.{index.html"],
+  theme: {
+    screens:{
+
+    },
+    extend: {
+      fontFamily:{
+        body:['nunito']
+      }
+    },
+  },
+  plugins: [],
+}
+
