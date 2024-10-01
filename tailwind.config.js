642a4984afadf24b99716bac8a7586158f743769
Zainab <134186742+salazee@users.noreply.github.com>
2024-10-01 16:53:39 +0100

    addn autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm:'480px',
+        md:'768px',
+        lg:'976px',
+        xl:'1440px',
+        // 'widescren': { 'raw': '(min-aspet-ratio:3/2)'},
+        // 'tallscreen':{ 'raw': '(max-aspect-ratio:13/20)'},
+      },
+    },
   },
   plugins: [],
 }
